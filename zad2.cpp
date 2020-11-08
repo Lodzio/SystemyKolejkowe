f0f3fdee4cf0b8b1c9823ebce3be7f7d749067a9
Bartosz <b.lodek@softwarepartner.pl>
2020-11-08 13:12:34 +0100

    rfactor

--- zad2.cpp
+++ zad2.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <functional>
 #include <cmath>
 #include <cstdlib>
+#include <random>
 #include <queue>
 #define M 4294967296
 #define A 134775813
 #define C 1
 long lastX = time(NULL);
+std::default_random_engine generator;
+std::normal_distribution<double> gaussDistribution(60.0,20.0);
 
 unsigned long LCG(){
     lastX = (A*lastX+C)%M;
@@ -26,15 +30,17 @@ double clearGauss(double x, int m, int l){
 double clearLaplace(double x,int m,int l){
     return (1/2.0*l)*exp(-abs(x - m)/l);
 }
-long GaussRand(int m, int l){
+int myGaussRand(int m, int l){
     while(true){
         double y = (LCG()%1000)/1000.0;
         double x = laplaceRand(m, l);
         if (clearGauss(x, m, l) > clearLaplace(x, m, l)*y*2){
-//            std::cout << x << std::endl;
             return x;
         }
     }
+}
+int gaussRand(){
+    return gaussDistribution(generator);
 }
 int exponentialRand(double lambda){
     double x = (LCG()%10000)/10000.0;
@@ -43,19 +49,14 @@ int exponentialRand(double lambda){
 
 std::queue < int > events;
 
-int getEventOperationTime(){
-    return 60;
-}
-
-int main(){
+double getAverageQueueTime(std::function< int() > getEventOperationTime, int N){
     int operationWaitSum = 0;
     int operatedEventsAmount = 0;
 
     int operationTimeLeft = getEventOperationTime();
     int nextEventTime = exponentialRand(1/120.0);
 
-    for (int i = 0; i < 100000; i++){
-//        std::cout << i << std::endl;
+    for (int i = 0; i < N; i++){
         int timeDiff = nextEventTime < operationTimeLeft ? nextEventTime: operationTimeLeft;
         operationTimeLeft -= timeDiff;
         nextEventTime -= timeDiff;
@@ -82,6 +83,26 @@ int main(){
             nextEventTime = exponentialRand(1/120.0);
         }
     }
-    std::cout << "final: " << std::endl;
-    std::cout << operationWaitSum/(double)operatedEventsAmount << std::endl;
+    return operationWaitSum/(double)operatedEventsAmount;
+}
+
+int main(){
+    auto constantOperationTime = []{return 60;};
+    auto UOperationTime = []{return ((LCG() % 10000)/10000.0)*120;};
+    auto exponentialOperationTime = []{return exponentialRand(1/60.0);};
+    auto gaussOperationTime = []{return gaussRand();};
+    auto myGaussOperationTime = []{return myGaussRand(60, 20);};
+    double result;
+
+    result = getAverageQueueTime(constantOperationTime, 10000000);
+    std::cout << "czas obslugi dokladnie 60s: " << result << std::endl;
+    result = getAverageQueueTime(UOperationTime, 10000000);
+    std::cout << "jest zmienną losową o rozkładzie U(0,120): " << result << std::endl;
+    result = getAverageQueueTime(exponentialOperationTime, 10000000);
+    std::cout << "jest zmienną losową o rozkładzie E(60): " << result << std::endl;
+    result = getAverageQueueTime(gaussOperationTime, 10000000);
+    std::cout << "jest zmienną losową o rozkładzie N(60,20) " << result << std::endl;
+    result = getAverageQueueTime(myGaussOperationTime, 100000);
+    std::cout << "jest zmienną losową o rozkładzie N(60,20) dla własnego generatora: " << result << std::endl;
+    return 0;
 }
